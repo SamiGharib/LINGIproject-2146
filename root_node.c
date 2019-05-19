00c838b618724d569c2937c0115410e3a43cefe9
gouya <yannick.goulleven@student.uclouvain.be>
2019-05-19 13:54:04 +0200

    routing protocol working with 1 root node and 1 sensor node

--- root_node.c
+++ root_node.c
@@ -8,70 +8,104 @@
 #include <string.h>
 #include "sys/timer.h"
 
-/*---------------------------------------------------------------------------*/
 PROCESS(root_node_process, "Root node");
 AUTOSTART_PROCESSES(&root_node_process);
+
+/* definition of macros */
+// maximum number of allowed children nodes
+#define MAX_CHILDREN 10
+// number of retransmissions in reliable unicast
+#define RETRANSMISSION 5
+// periodically send keep alive messages to children and parent nodes
+#define KEEP_ALIVE_TIME 15
+// periodically check for time outs
+#define KEEP_ALIVE_TIME_OUT 45
 /*---------------------------------------------------------------------------*/
 
-#define MAX_CHILDREN 10
-#define RETRANSMISSION_TIME 5
-#define ALIVE_TIME 15
-#define ALIVE_TIME_OUT 30
-
-
-// predefined messages that will be exchanged between nodes
+/* predefined messages that will be exchanged between nodes */
+// we accept to be the parent of a node
 static const char *IS_PARENT = "P";
+// we search for a node to be our parent
 static char *SEARCH_PARENT = "S";
+// the child node is still alive
 static char *CHILD_ALIVE = "C";
+// the parent node is still alive
 static char *PARENT_ALIVE = "A";
+/*---------------------------------------------------------------------------*/
 
-// define the boolean type in C -> to make code more understandable
-typedef enum { false, true } bool;
-
-// a timer for each children of the parent node
+/* Timers */
+// if this timer expires the corresponding child node is not alive anymore
 struct timer children_alive[MAX_CHILDREN];
+// periodically send alives messages to children and parent node
+struct timer this_alive;
+/*---------------------------------------------------------------------------*/
 
+/* Rime addresses */
+// list of all children nodes
 static linkaddr_t children_nodes[MAX_CHILDREN];
-
+// the address of this node
 static linkaddr_t this_node;
+/*---------------------------------------------------------------------------*/
 
-// a timer to send alive messages to each children
-struct timer alive;
-
-// we need reliable unicast and broadcast
+/* Rime structures */
+// structure for runicast
 static struct runicast_conn runicast;
+// structure for broadcast
 static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
 
 // what to do when a broadcast message is received
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
+
+  printf("broadcast message received from %d.%d: '%s'\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
   // extract message
   char *message = strtok((char *)packetbuf_dataptr()," ");
+  printf("The message is: %s\n", message);
+
   // a node searches for a parent
   if(strcmp(message, SEARCH_PARENT) == 0) {
-
     // search for an empty space
     int i;
     for(i=0; i < MAX_CHILDREN; i++) {
-      if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null)) {
+    if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) != 0) {
+
         // create the new child
         linkaddr_t new_child;
         new_child.u8[0] = from -> u8[0];
         new_child.u8[1] = from -> u8[1];
         children_nodes[i] = new_child;
-        // Send a packet to be the parent of the broadcasting node
+        // Send a runicast packet to the broadcasting node
         packetbuf_copyfrom(IS_PARENT, sizeof(&IS_PARENT));
-        runicast_send(&runicast, from, RETRANSMISSION_TIME);
+        runicast_send(&runicast, from, RETRANSMISSION);
         // create a timer for the new child node
-        timer_set(&(children_alive[i]), ALIVE_TIME_OUT*CLOCK_SECOND);
+        timer_set(&(children_alive[i]), KEEP_ALIVE_TIME_OUT*CLOCK_SECOND);
+        // need to break here
+        break;
       }
     }
   }
 }
 
+static void
+sent_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast message sent to %d.%d, retransmissions %d\n",
+	 to->u8[0], to->u8[1], retransmissions);
+}
+static void
+timedout_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast message timed out when sending to %d.%d, retransmissions %d\n",
+	 to->u8[0], to->u8[1], retransmissions);
+}
+
 // what to do when a unicast message is received
 static void runicast_recv(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
 {
+
+  printf("unicast message received from %d.%d: '%s'\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+  // extract message
   char *message = strtok((char *)packetbuf_dataptr()," ");
 
   // a keep alive message was received from a children node
@@ -79,7 +113,7 @@ static void runicast_recv(struct runicast_conn *c, const linkaddr_t *from, uint8
     // reset the keep alive timer
     int i;
     for(i=0; i < MAX_CHILDREN; i++) {
-      if(linkaddr_cmp(&(children_nodes[i]), from)) {
+      if(linkaddr_cmp(&(children_nodes[i]), from) != 0) {
         timer_restart(&(children_alive[i]));
       }
     }
@@ -87,7 +121,7 @@ static void runicast_recv(struct runicast_conn *c, const linkaddr_t *from, uint8
 }
 
 // constructs for broadcast and unicast
-static const struct runicast_callbacks runicast_call = {runicast_recv};
+static const struct runicast_callbacks runicast_call = {runicast_recv, sent_runicast, timedout_runicast};
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 
 /*---------------------------------------------------------------------------*/
@@ -101,11 +135,14 @@ PROCESS_THREAD(root_node_process, ev, data)
   PROCESS_BEGIN();
   clock_init();
 
-  timer_set(&alive, ALIVE_TIME*CLOCK_SECOND);
+  timer_set(&this_alive, KEEP_ALIVE_TIME*CLOCK_SECOND);
 
   // set the nodes rime addr
   this_node.u8[0] = linkaddr_node_addr.u8[0];
   this_node.u8[1] = linkaddr_node_addr.u8[1];
+
+  // counter variable used in for loops
+  int i;
 
   // Set up an identified best-effort broadcast connection
   broadcast_open(&broadcast, 129, &broadcast_call);
@@ -117,16 +154,16 @@ PROCESS_THREAD(root_node_process, ev, data)
     etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND*4));
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    if(timer_expired(&alive)) {
+    if(timer_expired(&this_alive)) {
       // check if children nodes still alive
-      int i;
       for(i=0; i < MAX_CHILDREN; i++) {
         // send a parent alive message to each children
-        if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) != 0) {
+        if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) == 0) {
           packetbuf_copyfrom(PARENT_ALIVE, sizeof(&PARENT_ALIVE));
-          runicast_send(&runicast, &children_nodes[i], RETRANSMISSION_TIME);
+          runicast_send(&runicast, &(children_nodes[i]), RETRANSMISSION);
         }
       }
+      timer_restart(&this_alive);
     }
 
     // check if children nodes still alive
--- sensor_node.c
+++ sensor_node.c
@@ -8,92 +8,126 @@
 #include <string.h>
 #include "sys/timer.h"
 
-/*---------------------------------------------------------------------------*/
 PROCESS(sensor_node_process, "Sensor node");
 AUTOSTART_PROCESSES(&sensor_node_process);
+
+/* definition of macros */
+// maximum number of allowed children nodes
+#define MAX_CHILDREN 10
+// number of retransmissions in reliable unicast
+#define RETRANSMISSION 5
+// periodically send keep alive messages to children and parent nodes
+#define KEEP_ALIVE_TIME 15
+// periodically check for time outs
+#define KEEP_ALIVE_TIME_OUT 45
 /*---------------------------------------------------------------------------*/
 
-// constants
-#define MAX_CHILDREN 10
-#define RETRANSMISSION_TIME 5
-#define ALIVE_TIME 15
-#define ALIVE_TIME_OUT 30
-
-// predefined messages that will be exchanged between nodes
+/* predefined messages that will be exchanged between nodes */
+// we accept to be the parent of a node
 static const char *IS_PARENT = "P";
+// we search for a node to be our parent
 static char *SEARCH_PARENT = "S";
+// the child node is still alive
 static char *CHILD_ALIVE = "C";
+// the parent node is still alive
 static char *PARENT_ALIVE = "A";
+/*---------------------------------------------------------------------------*/
 
+/* Timers */
+// if this timer expires the parent node is not alive anymore
+struct timer parent_alive;
+// if this timer expires the corresponding child node is not alive anymore
+struct timer children_alive[MAX_CHILDREN];
+// periodically send alives messages to children and parent node
+struct timer this_alive;
+/*---------------------------------------------------------------------------*/
 
-// define the boolean type in C -> to make code more understandable
-typedef enum { false, true } bool;
-// at the beginning the sensor node has no parent
-static bool has_parent = false;
+/* Rime addresses */
 // used to store the address of the parent node
 static linkaddr_t parent_node;
 // list of all children nodes
 static linkaddr_t children_nodes[MAX_CHILDREN];
 // the address of this node
 static linkaddr_t this_node;
-// timer used to check if parent still alive
+/*---------------------------------------------------------------------------*/
 
-
-struct timer parent_alive;
-struct timer children_alive[MAX_CHILDREN];
-
-
+/* Rime structures */
+// structure for runicast
 static struct runicast_conn runicast;
+// structure for broadcast
 static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
+
+/* Other global variables */
+// keeps track if this node has currently a parent or not
+static int has_parent = 0;
+/*---------------------------------------------------------------------------*/
 
 
-// what to do when a broadcast message is received
+
+/*
+ * Function that is called when a broadcast message is received.
+ * If the send message was a SEARCH_PARENT message and we have a parent node
+ * then we reply to the message with an IS_PARENT message via runicast.
+ * We also add the node as a child
+ */
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-
   printf("broadcast message received from %d.%d: '%s'\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
   // extract message
   char *message = strtok((char *)packetbuf_dataptr()," ");
   // a node searches for a parent -> accept only if this node has already a parent
-  if(strcmp(message, SEARCH_PARENT) == 0 && has_parent == true) {
-    // search for an empty space
+  if(strcmp(message, SEARCH_PARENT) == 0 && has_parent == 1) {
+    // check if we have space for one more child node
     int i;
     for(i=0; i < MAX_CHILDREN; i++) {
-      if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null)) {
+      if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) != 0) {
         // create the new child
         linkaddr_t new_child;
         new_child.u8[0] = from -> u8[0];
         new_child.u8[1] = from -> u8[1];
         children_nodes[i] = new_child;
-        // Send a packet to be the parent of the broadcasting node
+        // Send a runicast packet to the broadcasting node
         packetbuf_copyfrom(IS_PARENT, sizeof(&IS_PARENT));
-        runicast_send(&runicast, from, RETRANSMISSION_TIME);
+        runicast_send(&runicast, from, RETRANSMISSION);
         // create a timer for the new child node
-        timer_set(&(children_alive[i]), ALIVE_TIME_OUT*CLOCK_SECOND);
+        timer_set(&(children_alive[i]), KEEP_ALIVE_TIME_OUT*CLOCK_SECOND);
+        break;
       }
     }
   }
 }
 
-// what to do when a unicast message is received
+
+
+/*
+ * Function that is called when a runicast message is received.
+ * If the send message was a IS_PARENT message we have a parent node and we can
+ * start a timer for the parent node
+ * If the send message was a CHILD_ALIVE message then the corresponding child
+ * node is alive and we can restart the timer.
+ * If the send message was a PARENT_ALIVE message then the parent node is still
+ * alive and we can restert the timer.
+ */
 static void runicast_recv(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
 {
 
-  printf("unicast message received from %d.%d\n", from->u8[0], from->u8[1]);
+  printf("unicast message received from %d.%d: '%s'\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+  // extract the message
   char *message = strtok((char *)packetbuf_dataptr()," ");
 
   // Another node wants to be our parent
   if(strcmp(message, IS_PARENT) == 0) {
     // if we still have no parent, accept to be this node's child
-    if(has_parent == false) {
+    if(has_parent == 0) {
       // keep track of the parent node
-      has_parent = true;
+      has_parent = 1;
       parent_node = *from;
       // alert the parent node that we are his child now
       packetbuf_copyfrom(CHILD_ALIVE, sizeof(&CHILD_ALIVE));
-      runicast_send(&runicast, from, RETRANSMISSION_TIME);
+      runicast_send(&runicast, from, RETRANSMISSION);
       // set a keep alive timer for the parent node
-      timer_set(&parent_alive, ALIVE_TIME*CLOCK_SECOND);
+      timer_set(&parent_alive, KEEP_ALIVE_TIME_OUT*CLOCK_SECOND);
     }
   }
 
@@ -102,21 +136,33 @@ static void runicast_recv(struct runicast_conn *c, const linkaddr_t *from, uint8
     // reset the keep alive timer
     int i;
     for(i=0; i < MAX_CHILDREN; i++) {
-      if(linkaddr_cmp(&(children_nodes[i]), from)) {
+      if(linkaddr_cmp(&(children_nodes[i]), from) != 0) {
         timer_restart(&(children_alive[i]));
       }
     }
   }
 
   // a keep alive message was received from parent node
-  if(strcmp(message, PARENT_ALIVE) == 0 && linkaddr_cmp(&parent_node, from)) {
+  if(strcmp(message, PARENT_ALIVE) == 0 && linkaddr_cmp(&parent_node, from) != 0) {
     timer_restart(&parent_alive);
   }
 }
 
+static void
+sent_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast message sent to %d.%d, retransmissions %d\n",
+	 to->u8[0], to->u8[1], retransmissions);
+}
+static void
+timedout_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast message timed out when sending to %d.%d, retransmissions %d\n",
+	 to->u8[0], to->u8[1], retransmissions);
+}
 
 // constructs for broadcast and unicast
-static const struct runicast_callbacks runicast_call = {runicast_recv};
+static const struct runicast_callbacks runicast_call = {runicast_recv, sent_runicast, timedout_runicast};
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 
 
@@ -136,13 +182,17 @@ PROCESS_THREAD(sensor_node_process, ev, data)
   this_node.u8[0] = linkaddr_node_addr.u8[0];
   this_node.u8[1] = linkaddr_node_addr.u8[1];
 
-
   // Set up an identified best-effort broadcast connection
   // 129 -> channel
   // &boradcast_call -> a structure with function pointers to functions that will be called when a packet has been received
   broadcast_open(&broadcast, 129, &broadcast_call);
   runicast_open(&runicast, 144, &runicast_call);
 
+  // counter variable used in for loops
+  int i;
+
+  // set alive counter
+  timer_set(&this_alive, KEEP_ALIVE_TIME*CLOCK_SECOND);
 
   while(1) {
 
@@ -152,39 +202,51 @@ PROCESS_THREAD(sensor_node_process, ev, data)
 
     // check if parent still alive
     if(timer_expired(&parent_alive)) {
-      printf("Lost connection to parent node");
-      has_parent = false;
+      printf("No connection to parent node\n");
+      has_parent = 0;
       parent_node = linkaddr_null;
     }
 
     // check if children nodes still alive
-   int i;
    for(i=0; i < MAX_CHILDREN; i++) {
-
-     if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) != 0) {
-       packetbuf_copyfrom(PARENT_ALIVE, sizeof(&PARENT_ALIVE));
-       runicast_send(&runicast, &children_nodes[i], RETRANSMISSION_TIME);
-     }
-
      if(timer_expired(&(children_alive[i]))) {
        children_nodes[i] = linkaddr_null;
      }
    }
 
+   if(timer_expired(&this_alive)) {
+
+     // send keep alive message to parent node
+     if(has_parent == 1) {
+       // send keep alive message to parent node
+       packetbuf_copyfrom(CHILD_ALIVE, sizeof(&CHILD_ALIVE));
+       runicast_send(&runicast, &parent_node, RETRANSMISSION);
+     }
+      for(i=0; i < MAX_CHILDREN; i++) {
+        if(linkaddr_cmp(&(children_nodes[i]), &linkaddr_null) == 0) {
+          packetbuf_copyfrom(PARENT_ALIVE, sizeof(&PARENT_ALIVE));
+          runicast_send(&runicast, &(children_nodes[i]), RETRANSMISSION);
+        }
+      }
+      timer_restart(&this_alive);
+    }
+
     // the node has no parent node
-    if(has_parent == false) {
+    if(has_parent == 0) {
       // Copy from external data into the packetbuffer
       packetbuf_copyfrom(SEARCH_PARENT, sizeof(&SEARCH_PARENT));
       // Send an identified best-effort broadcast packet
       broadcast_send(&broadcast);
-      printf("Broadcasted No Parent packet");
+      printf("Broadcasted No Parent packet\n");
 
     }
-
     // the node has a parent node
     else {
       // send data
-      printf("Could send data right now");
+      // alert the parent node that we are his child now
+      //packetbuf_copyfrom("AA", 2);
+      //runicast_send(&runicast, &parent_node, RETRANSMISSION);
+      printf("Could send data right now\n");
     }
 
 
